52513372266363ee4849d403b783a651a78b098c
elizabeth-anderson <student@elizabeths-computer.local>
2018-04-24 12:34:59 -0500

    edit table view button and actions beth

--- digitalReciept/FoldersTableViewController.swift
+++ digitalReciept/FoldersTableViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-// do folders ast, camera first
 class FoldersTableViewController: UITableViewController {
 
     var folders = [String]()
@@ -18,96 +17,51 @@ class FoldersTableViewController: UITableViewController {
 
          folders = ["Resturants","Travel","Shopping"]
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+      
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
         return folders.count
     }
-
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "folderCell", for: indexPath)
-
         cell.textLabel!.text = folders[indexPath.row]
-
         return cell
     }
     
-    
-    @IBAction func cancel(segue:UIStoryboardSegue) {
-        
-    }
-    
-    @IBAction func done(segue: UIStoryboardSegue) {
-        let addFolderVC = segue.source as! addFolderViewController
-                newFolder = addFolderVC.name
-                folders.append(newFolder)
-    }
-    
-    
-   
-//    @IBAction func done(segue:UIStoryboardSegue)
-//    {
-//        var addFolderVC = segue.source as! addFolderViewController
-//        newFolder = addFolderVC.name
-//        
-//        folders.append(newFolder)
-//    }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
+    
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
+    
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
+  
 
     /*
     // MARK: - Navigation
