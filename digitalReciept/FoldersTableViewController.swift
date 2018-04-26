f0e08b152de3f9916b7ef7c64f083fa618ea27e8
elizabeth-anderson <student@elizabeths-computer.local>
2018-04-26 12:40:51 -0500

    ADD FOLDER!!!

--- digitalReciept/FoldersTableViewController.swift
+++ digitalReciept/FoldersTableViewController.swift
@@ -9,15 +9,13 @@
 import UIKit
 class FoldersTableViewController: UITableViewController {
 
+ 
     var folders = [String]()
-    var newFolder: String = ""
+    var newFolder = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
          folders = ["Resturants","Travel","Shopping"]
-        
-      
         self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
 
@@ -50,8 +48,10 @@ class FoldersTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            let folder = folders.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
+            folders.append(newFolder)
         }    
     }
     
@@ -61,6 +61,35 @@ class FoldersTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         return true
     }
+    
+
+//        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewFolder(_:)))
+    @IBAction func addButton(_ sender: UIBarButtonItem)
+    {
+        insertNewFolder()
+        tableView.reloadData()
+    }
+    
+     func insertNewFolder()
+    {
+        let alert = UIAlertController(title: "Add Folder", message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Folder"
+        }
+        let cancelAction = UIAlertAction(title:"cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        let insertAction = UIAlertAction(title: "add", style: .default) { (action) in
+            let folderTextField = alert.textFields![0] as UITextField
+            let newFolder = folderTextField.text
+           
+            self.folders.append(newFolder!)
+             self.tableView.reloadData()
+        }
+          alert.addAction(insertAction)
+        present(alert, animated: true, completion: nil)
+        
+    }
+       
   
 
     /*
