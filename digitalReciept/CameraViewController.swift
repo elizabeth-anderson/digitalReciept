b8b68b4a41cf04e7f5ddfa0219fa4f63b9e6fcc6
elizabeth-anderson <student@elizabeths-computer.local>
2018-05-22 13:00:29 -0500

    cleaned code

--- digitalReciept/CameraViewController.swift
+++ digitalReciept/CameraViewController.swift
@@ -10,62 +10,42 @@ import UIKit
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
-
+    
     @IBOutlet weak var photoLibary: UIButton!
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
-    //@IBOutlet weak var collectionView: UICollectionView!
     
-    
-     let picker = UIImagePickerController()
+    let picker = UIImagePickerController()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
-        
     }
-    
-    
-   
-    
-    
-    
     
     @IBAction func photoLibraryAction(sender:UIButton)
     {
-//        let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .photoLibrary
         present(picker, animated: true, completion: nil)
     }
-   // calls camera button
+    
     @IBAction func cameraButtonAction(sender: UIButton)
     {
-//        let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .camera
-//        present(picker, animated: true, completion: nil)
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
         {
             picker.sourceType = UIImagePickerControllerSourceType.camera
             present(picker, animated: true, completion: nil)
         }
-        
-        
     }
-    // enables choising an image 
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
     {
-        //let picker = UIImagePickerController()
         picker.dismiss(animated: true)
         {
             let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
             self.imageView.image = selectedImage
         }
-//      imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage;dismiss(animated: true, completion: nil)
-      //collectionView.cell = info[UIImagePickerControllerOriginalImage] as? UIImage;dismiss(animated: true, completion: nil)
-      
     }
-
 }
--- digitalReciept/FoldersTableViewController.swift
+++ digitalReciept/FoldersTableViewController.swift
@@ -5,34 +5,29 @@
 //  Created by Elizabeth on 4/16/18.
 //  Copyright © 2018 Elizabeth. All rights reserved.
 //
-//type a random comment
 import UIKit
 class FoldersTableViewController: UITableViewController {
-
- 
+    
     var folders = [String]()
     var newFolder = String()
     
-    override func viewDidLoad() { //nananana
+    override func viewDidLoad() {
         super.viewDidLoad()
-         folders = ["Resturants","Travel","Shopping"]
+        folders = ["Restaurants","Travel","Shopping"]
         self.navigationItem.leftBarButtonItem = self.editButtonItem
         editButtonItem.tintColor = UIColor.black
-      
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-       
+        
     }
-
-    // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return folders.count
@@ -47,7 +42,7 @@ class FoldersTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-    // enables editing and adding to the table view
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             let folder = folders.remove(at: indexPath.row)
@@ -59,20 +54,18 @@ class FoldersTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
     }
-    // allows moving rows
+    
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
-
-//        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewFolder(_:)))
     @IBAction func addButton(_ sender: UIBarButtonItem)
     {
         insertNewFolder()
         tableView.reloadData()
     }
-    //alert that adds a folder with title 
-     func insertNewFolder()
+    
+    func insertNewFolder()
     {
         let alert = UIAlertController(title: "Add Folder", message: nil, preferredStyle: .alert)
         alert.addTextField { (textField) in
@@ -83,25 +76,11 @@ class FoldersTableViewController: UITableViewController {
         let insertAction = UIAlertAction(title: "add", style: .default) { (action) in
             let folderTextField = alert.textFields![0] as UITextField
             let newFolder = folderTextField.text
-           
+            
             self.folders.append(newFolder!)
-             self.tableView.reloadData()
+            self.tableView.reloadData()
         }
-          alert.addAction(insertAction)
+        alert.addAction(insertAction)
         present(alert, animated: true, completion: nil)
-        
     }
-       
-  
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
